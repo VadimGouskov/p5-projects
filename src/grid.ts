969a0f1ef40185aa493e081fe22fdb04318ecd5d
VadimGouskov <vadimgouskov@hotmail.com>
2021-12-16 22:11:08 +0100

    open-grid init commit

---
+++ src/grid.ts
@@ -0,0 +1,83 @@
+export class GridPoint {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+export class Grid {
+    amountX: number;
+    amountY: number;
+
+    private grid: GridPoint[][] = [[]];
+
+    constructor(amountX: number, amountY: number, width: number, height: number) {
+        this.amountX = amountX;
+        this.amountY = amountY;
+
+        const stepX = width / (amountX - 1);
+        const stepY = height / (amountY - 1);
+
+        // initialize grid
+        for (let i = 0; i < amountX; i++) {
+            this.grid[i] = [];
+            for (let j = 0; j < amountX; j++) {
+                this.grid[i][j] = new GridPoint(j * stepX, i * stepY);
+            }
+        }
+    }
+
+    get flat(): GridPoint[] {
+        return this.grid.reduce((acc, val) => acc.concat(val), []);
+    }
+
+    get(): GridPoint[][] {
+        return this.grid;
+    }
+
+    set(grid: GridPoint[][]): void {
+        this.grid = grid;
+    }
+
+    getPoint(xIndex: number, yindex: number): GridPoint {
+        return this.grid[yindex][xIndex];
+    }
+
+    getRandomPoint(): GridPoint {
+        try {
+            const xIndex = Math.floor(Math.random() * (this.amountX - 1));
+            const yIndex = Math.floor(Math.random() * (this.amountY - 1));
+            return this.grid[yIndex][xIndex];
+        } catch (err) {
+            debugger;
+            return new GridPoint(0, 0);
+        }
+    }
+
+    slice(startXIndex: number, stopXIndex: number, startYIndex: number, stopYIndex: number): GridPoint[][] {
+        return this.grid.slice(startYIndex, stopYIndex + 1).map((i) => i.slice(startXIndex, stopXIndex + 1));
+    }
+
+    draw(func: (x: number, y: number) => void): void {
+        this.flat.forEach((point) => func(point.x, point.y));
+    }
+}
+
+export class CountingGrid {
+    grid: Grid;
+    pointCounters: number[][] = [[]];
+    constructor(grid: Grid) {
+        this.grid = grid;
+
+        const maxRows = grid.amountY;
+        const maxCols = grid.amountX;
+        let i;
+        for (i = 0; i < maxRows; i++) {
+            this.pointCounters[i] = [];
+            this.pointCounters[i].length = maxCols;
+            this.pointCounters[i].fill(0);
+        }
+    }
+}
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,19 +1,118 @@
 import p5 from 'p5';
+import { FileClient, getCanvasImage } from 'p5-file-client';
+import { Grid, GridPoint } from './grid';
 
 const s = (p: p5) => {
     const CANVAS_WIDTH = 500;
     const CANVAS_HEIGHT = 500;
 
+    const MAIN_GRID_DENSITY = 41;
+    const SHAPES_AMOUNT = 40;
+
+    let fileClient: FileClient;
+    let mainGrid: Grid;
+
     p.setup = () => {
         const canvas = p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
         canvas.parent('sketch');
+        p.ellipseMode(p.CENTER);
+        p.colorMode(p.HSB, 360, 100, 100);
+        p.noLoop();
+
+        fileClient = new FileClient(
+            undefined,
+            undefined,
+            '/home/vadim/Projects/creative-coding/p5-projects/open-grid/progress',
+        );
+
+        mainGrid = new Grid(MAIN_GRID_DENSITY, MAIN_GRID_DENSITY, CANVAS_WIDTH, CANVAS_HEIGHT);
     };
 
     p.draw = () => {
         p.background(255);
         p.fill(0);
-        p.ellipseMode(p.CENTER);
-        p.ellipse(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, 50, 50);
+
+        p.push();
+        p.fill('#ff0000');
+        p.noStroke();
+        mainGrid.draw((x: number, y: number) => p.ellipse(x, y, 1, 1));
+        p.pop();
+
+        p.fill('#0000ff');
+        p.noStroke();
+
+        Array(SHAPES_AMOUNT)
+            .fill(0)
+            .forEach(() => {
+                p.fill(randomInt(0, 361), 100, 100);
+                const indexYStart = randomInt(0, mainGrid.amountY - 4 - 1);
+                const indexYEnd = randomInt(indexYStart + 1, mainGrid.amountY - 1);
+                const indexXStart = randomInt(0, mainGrid.amountX - 4 - 1);
+                const indexXEnd = randomInt(indexXStart + 1, mainGrid.amountX - 1);
+                debugger;
+                const slice = mainGrid.slice(indexYStart, indexYEnd, indexXStart, indexXEnd);
+                const slicedGrid = new Grid(slice.length, slice[0].length, 0, 0);
+                slicedGrid.set(slice);
+
+                drawRandomShape(slicedGrid);
+            });
+
+        // EXPORT
+        const image64 = getCanvasImage('sketch');
+        fileClient.exportImage64(image64, '.png', 'open-grid');
+    };
+
+    const drawRandomShape = (grid: Grid) => {
+        const q1 = getPointFromSlice(grid, 0, Math.floor(grid.amountY / 2), 0, Math.floor(grid.amountX / 2));
+        const q2 = getPointFromSlice(
+            grid,
+            0,
+            Math.floor(grid.amountY / 2),
+            Math.floor(grid.amountX / 2) + 1,
+            grid.amountX - 1,
+        );
+        const q3 = getPointFromSlice(
+            grid,
+            Math.floor(grid.amountY / 2),
+            grid.amountY - 1,
+            Math.floor(grid.amountX / 2) + 1,
+            grid.amountX - 1,
+        );
+        const q4 = getPointFromSlice(
+            grid,
+            Math.floor(grid.amountY / 2),
+            grid.amountY - 1,
+            0,
+            Math.floor(grid.amountX / 2),
+        );
+
+        p.beginShape();
+        [q1, q2, q3, q4].forEach((point) => {
+            p.vertex(point.x, point.y);
+        });
+        p.endShape(p.CLOSE);
+    };
+
+    const getPointFromSlice = (
+        grid: Grid,
+        startYIndex: number,
+        stopYIndex: number,
+        startXIndex: number,
+        stopXIndex: number,
+    ): GridPoint => {
+        const slice = grid.slice(startYIndex, stopYIndex, startXIndex, stopXIndex);
+        const slicedGrid = new Grid(slice.length, slice[0].length, 0, 0);
+        slicedGrid.set(slice);
+        const randomPoint = slicedGrid.getRandomPoint();
+        return randomPoint;
+    };
+
+    const randomInt = (min: number, max: number): number => {
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    p.mouseClicked = () => {
+        p.redraw();
     };
 };
 
