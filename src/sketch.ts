8f86bc63713b4b5f62b3e8f7b6e2a356c5d32efc
VadimGouskov <vadimgouskov@hotmail.com>
2021-12-27 11:58:54 +0100

    added random slice boundries

--- src/sketch.ts
+++ src/sketch.ts
@@ -9,10 +9,9 @@ const s = (p: p5) => {
     const SHAPE_GRID_DENSITY = 20;
     const DOT_GRID_DENSITY = 40;
     const SHAPE_GRID_AMOUNT = 3;
-    const DOT_GRID_AMOUNT = 2;
+    const DOT_GRID_AMOUNT = 1;
 
-    // Should be minum 2 because the grid gets divided in quadrants for drawing the shape
-    const MINIMUM_SLICE_WIDTH = 4;
+    const LINE_THICKNESS = 20;
 
     let fileClient: FileClient;
     let shapeGrid: Grid;
@@ -37,43 +36,52 @@ const s = (p: p5) => {
 
     p.draw = () => {
         p.background(255);
-        p.fill(0);
-
-        p.push();
-        p.fill('#ff0000');
-        p.noStroke();
-        // shapeGrid.draw((x: number, y: number) => p.ellipse(x, y, 1, 1));
-        p.pop();
 
         p.fill('#0000ff');
         p.noStroke();
 
-        // GRIDS
-        Array(DOT_GRID_AMOUNT)
-            .fill(0)
-            .forEach(() => {
-                const slicedGrid = randomGridSlice(dotGrid);
+        // DOTS
+        iterator(DOT_GRID_AMOUNT, () => {
+            const slicedGrid = randomGridSlice(dotGrid, { minWidth: 4, maxWidth: 8, minHeight: 8, maxHeight: 10 });
+            p.fill(0, 0, 0);
+            slicedGrid.draw((x: number, y: number) => p.ellipse(x, y, 3, 3));
+        });
 
-                p.fill(0, 0, 0);
-                slicedGrid.draw((x: number, y: number) => p.ellipse(x, y, 3, 3));
-            });
         // SHAPES
-        Array(SHAPE_GRID_AMOUNT)
-            .fill(0)
-            .forEach(() => {
-                p.fill(randomInt(0, 361), 100, 100);
-                p.noStroke();
-                const slicedGrid = randomGridSlice(shapeGrid);
+        iterator(SHAPE_GRID_AMOUNT, () => {
+            const slicedGrid = randomGridSlice(shapeGrid, { minWidth: 4, maxWidth: 8, minHeight: 8, maxHeight: 8 });
 
-                drawRandomShape(slicedGrid);
+            // Draw the bg
+            let bgGraphics = p.createGraphics(CANVAS_WIDTH, CANVAS_HEIGHT);
+            bgGraphics.colorMode(bgGraphics.HSB, 360, 100, 100);
+            if (chance(0.3)) {
+                bgGraphics = drawLineBG(bgGraphics);
+            } else {
+                bgGraphics.background(randomInt(0, 361), 75, 100);
+            }
+
+            // Draw the mask
+            const maskGraphics = p.createGraphics(CANVAS_WIDTH, CANVAS_HEIGHT);
+            maskGraphics.fill(0);
+            maskGraphics.beginShape();
+            getRandomShape(slicedGrid).forEach((point) => {
+                maskGraphics.vertex(point.x, point.y);
             });
+            maskGraphics.endShape(p.CLOSE);
+
+            // Mask the BG
+            const bgImage = bgGraphics.get();
+            bgImage.mask(maskGraphics.get());
+            //Draw the masked image
+            p.image(bgImage, 0, 0);
+        });
 
         // EXPORT
         const image64 = getCanvasImage('sketch');
         fileClient.exportImage64(image64, '.png', 'open-grid');
     };
 
-    const drawRandomShape = (grid: Grid) => {
+    const getRandomShape = (grid: Grid): Array<GridPoint> => {
         const q1 = getPointFromSlice(grid, 0, Math.floor(grid.amountY / 2), 0, Math.floor(grid.amountX / 2));
         const q2 = getPointFromSlice(
             grid,
@@ -97,11 +105,7 @@ const s = (p: p5) => {
             Math.floor(grid.amountX / 2),
         );
 
-        p.beginShape();
-        [q1, q2, q3, q4].forEach((point) => {
-            p.vertex(point.x, point.y);
-        });
-        p.endShape(p.CLOSE);
+        return [q1, q2, q3, q4];
     };
 
     const getPointFromSlice = (
@@ -118,13 +122,31 @@ const s = (p: p5) => {
         return randomPoint;
     };
 
-    const randomGridSlice = (grid: Grid): Grid => {
+    const randomGridSlice = (
+        grid: Grid,
+        {
+            minWidth,
+            maxWidth,
+            minHeight,
+            maxHeight,
+        }: {
+            minWidth: number;
+            maxWidth: number;
+            minHeight: number;
+            maxHeight: number;
+        },
+    ): Grid => {
         // Get a portion a the grid to construct the shape
         // TODO better adjustable max width
-        const indexYStart = randomInt(1, grid.amountY - 2 * MINIMUM_SLICE_WIDTH - 1);
-        const indexYEnd = randomInt(indexYStart + MINIMUM_SLICE_WIDTH, grid.amountY - 1);
-        const indexXStart = randomInt(1, grid.amountX - 2 * MINIMUM_SLICE_WIDTH - 1);
-        const indexXEnd = randomInt(indexXStart + MINIMUM_SLICE_WIDTH, grid.amountX - 1);
+        const indexYStart = randomInt(1, grid.amountY - minHeight - 1);
+        const indexYMinEnd = indexYStart + minHeight;
+        const indexYMaxEnd = Math.min(indexYStart + maxHeight, grid.amountY - 1);
+        const indexYEnd = randomInt(indexYMinEnd, indexYMaxEnd);
+
+        const indexXStart = randomInt(1, grid.amountX - minWidth - 1);
+        const indexXMinEnd = indexXStart + minWidth;
+        const indexXMaxEnd = Math.min(indexXStart + maxWidth, grid.amountX - 1);
+        const indexXEnd = randomInt(indexXMinEnd, indexXMaxEnd);
 
         const slice = grid.slice(indexYStart, indexYEnd, indexXStart, indexXEnd);
         const slicedGrid = new Grid(slice[0].length, slice.length, 0, 0);
@@ -132,10 +154,32 @@ const s = (p: p5) => {
         return slicedGrid;
     };
 
+    // GRAPHICS
+    const drawLineBG = (grapics: p5.Graphics): p5.Graphics => {
+        const hue = randomInt(0, 361);
+        // grapics.background(hue, 50, 100);
+        grapics.fill(hue, 100, 100);
+        grapics.noStroke();
+        iterator(Math.floor(grapics.height / LINE_THICKNESS), (index: number) => {
+            grapics.rect(0, 2 * index * LINE_THICKNESS, grapics.width, LINE_THICKNESS);
+        });
+        return grapics;
+    };
+
+    // UTILITES
     const randomInt = (min: number, max: number): number => {
         return Math.floor(Math.random() * (max - min)) + min;
     };
 
+    const iterator = (amount: number, func: (index: number) => void) => {
+        Array(amount)
+            .fill(0)
+            .forEach((e, i) => func(i));
+    };
+
+    const chance = (n: number): boolean => Math.random() < n;
+
+    // EVENTS
     p.mouseClicked = () => {
         p.redraw();
     };
