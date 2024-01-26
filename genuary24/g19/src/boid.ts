9b17c3658cbc21a20995130d9bfdd4795dbfb1e8
Vadim Gouskov <vadim@acostudio.be>
2024-01-26 17:47:35 +0100

    feat: implemented flocking

--- genuary24/g19/src/boid.ts
+++ genuary24/g19/src/boid.ts
@@ -1,7 +1,12 @@
 import Victor from "victor";
 import { scale } from "./helpers";
 
-type BoidOptions = { x: number; y: number; maxSpeed?: number };
+type BoidOptions = {
+  x: number;
+  y: number;
+  maxSpeed?: number;
+  velocity?: Victor;
+};
 
 export class Boid {
   loc: Victor;
@@ -13,9 +18,13 @@ export class Boid {
   arrivalDistance: number;
   arrivalDampening: number;
 
+  visionDistance: number;
+
+  desiredSeparation: number;
+
   constructor({ x, y, ...options }: BoidOptions) {
     this.loc = new Victor(x, y);
-    this.vel = new Victor(0, 0);
+    this.vel = options.velocity || new Victor(0, 0);
     this.acc = new Victor(0, 0);
 
     this.maxSpeed = options.maxSpeed || 4;
@@ -24,6 +33,9 @@ export class Boid {
 
     this.arrivalDistance = 50;
     this.arrivalDampening = 0.01;
+
+    this.visionDistance = 50;
+    this.desiredSeparation = 25;
   }
 
   applyForce = (force: Victor) => {
@@ -53,7 +65,7 @@ export class Boid {
 
     const steer = this.limitVector(desired, this.maxForce);
 
-    this.applyForce(steer);
+    return steer;
   };
 
   limitVector = (v: Victor, max: number) => {
@@ -74,4 +86,97 @@ export class Boid {
   get angle() {
     return this.vel.angleDeg() + 90;
   }
+
+  flock = (boids: Boid[]) => {
+    const sep = this.separate(boids);
+    const ali = this.align(boids);
+    const coh = this.cohesion(boids);
+
+    sep.multiplyScalar(1.5);
+    ali.multiplyScalar(1.5);
+    coh.multiplyScalar(1.5);
+
+    this.applyForce(ali);
+    this.applyForce(sep);
+  };
+
+  wrap = (width: number, height: number) => {
+    if (this.loc.x < -this.desiredSeparation) {
+      this.loc.x = width + this.desiredSeparation;
+    }
+    if (this.loc.y < -this.desiredSeparation) {
+      this.loc.y = height + this.desiredSeparation;
+    }
+    if (this.loc.x > width + this.desiredSeparation) {
+      this.loc.x = -this.desiredSeparation;
+    }
+    if (this.loc.y > height + this.desiredSeparation) {
+      this.loc.y = -this.desiredSeparation;
+    }
+  };
+
+  separate = (boids: Boid[]) => {
+    const sum = new Victor(0, 0);
+
+    let count = 0;
+
+    for (let boid of boids) {
+      const d = this.loc.distance(boid.loc);
+      if (d > 0 && d < this.desiredSeparation) {
+        const diff = this.loc.clone().subtract(boid.loc);
+        diff.normalize();
+        diff.divideScalar(d);
+        sum.add(diff);
+        count++;
+      }
+    }
+
+    if (count === 0) return new Victor(0, 0);
+
+    sum.divideScalar(count);
+    sum.normalize();
+    sum.multiplyScalar(this.maxSpeed);
+    let steer = sum.subtract(this.vel);
+    steer = this.limitVector(steer, this.maxForce);
+    return steer;
+  };
+
+  align = (boids: Boid[]) => {
+    const sum = new Victor(0, 0);
+    let count = 0;
+    for (let boid of boids) {
+      const d = this.loc.distance(boid.loc);
+      if (d > 0 && d < this.visionDistance) {
+        sum.add(boid.vel);
+        count++;
+      }
+    }
+
+    if (count === 0) return new Victor(0, 0);
+
+    sum.divideScalar(count); // average velocity of all neighbors
+    sum.normalize();
+    sum.multiplyScalar(this.maxSpeed); // set magnitude to maxSpeed
+
+    let steer = sum.subtract(this.vel);
+    steer = this.limitVector(steer, this.maxForce);
+    return steer;
+  };
+
+  cohesion = (boids: Boid[]) => {
+    const sum = new Victor(0, 0);
+    let count = 0;
+    for (let boid of boids) {
+      const d = this.loc.distance(boid.loc);
+      if (d > 0 && d < this.visionDistance) {
+        sum.add(boid.loc);
+        count++;
+      }
+    }
+
+    if (count === 0) return new Victor(0, 0);
+
+    sum.divideScalar(count); // average location of all neighbors
+    return this.seek(sum); // seek that location
+  };
 }
--- genuary24/g19/src/main.ts
+++ genuary24/g19/src/main.ts
@@ -7,7 +7,6 @@ import {
   Polygon,
 } from "@svgdotjs/svg.js";
 
-import { createGrid } from "pretty-grid";
 import { Boid } from "./boid";
 import Victor from "victor";
 
@@ -17,9 +16,7 @@ const HEIGHT = 700;
 var draw = SVG().addTo("body").size(WIDTH, HEIGHT);
 
 const BOID_AMOUNT = 50;
-
-const target = new Victor(500, 100);
-const circle = draw.circle(10).attr({ cx: target.x, cy: target.y }).fill("red");
+const MAX_BOID_SPEED = 5;
 
 const boids: Boid[] = [];
 const skins: Polygon[] = [];
@@ -27,7 +24,21 @@ for (let i = 0; i < BOID_AMOUNT; i++) {
   const x = Math.random() * WIDTH;
   const y = Math.random() * HEIGHT;
 
-  boids.push(new Boid({ x, y, maxSpeed: 4 }));
+  const startVelocityX = Math.random() * 2 - 1; // map range  0-1 to (-1)-1
+  const startVelocityY = Math.random() * 2 - 1;
+  const startVelocity = new Victor(
+    startVelocityX,
+    startVelocityY
+  ).multiplyScalar(MAX_BOID_SPEED);
+
+  boids.push(
+    new Boid({
+      x,
+      y,
+      maxSpeed: MAX_BOID_SPEED,
+      velocity: startVelocity,
+    })
+  );
   skins.push(
     draw
       .polygon("0,-10 -10,10 10,10")
@@ -39,11 +50,14 @@ for (let i = 0; i < BOID_AMOUNT; i++) {
 
 const loop = () => {
   for (let i = 0; i < BOID_AMOUNT; i++) {
+    // flocking
     const boid = boids[i];
     const skin = skins[i];
-    boid.seek(target.clone());
+    boid.flock(boids);
+    boid.wrap(WIDTH, HEIGHT);
     boid.update();
-    // skin.rotate(boid.direction);
+
+    // draw the boids
     skin.x(boid.x).y(boid.y);
     skin.transform({ rotate: boid.angle });
   }
