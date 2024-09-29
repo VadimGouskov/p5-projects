2564a39de95578d66f5d39a18b14fe1d9b4c6498
Vadim Gouskov <vadim@acostudio.be>
2024-09-29 18:38:20 +0200

    feat: added patches

--- three.js/ui_extended/src/main.ts
+++ three.js/ui_extended/src/main.ts
@@ -26,41 +26,20 @@ let A = Math.PI / 200,
   N = 5;
 
 // BOX GRID
-const COLS = 14;
-const ROWS = 14;
-const PLANE_SIZE = 10;
+const COLS = 40;
+const ROWS = 40;
+const PLANE_SIZE = 20;
 const BOX_SIZE = PLANE_SIZE / (COLS - 1);
 let AMP = BOX_SIZE * 3;
 const MIN_BOX_HEIGHT = 0.5;
 const STEP_SIZE = 0.001;
-
-const a = new RangeControl("A", "controls", {
-  min: 0,
-  max: Math.PI / 4,
-  step: Math.PI / 100000,
-  value: Math.PI / 200,
-});
-
-const b = new RangeControl("B", "controls", {
-  min: 0,
-  max: 0.01,
-  step: 0.00005,
-  value: 0.05,
-});
 
 // General Controls
 const amp = new RangeControl("AMP", "general-controls", {
   min: 0,
   max: 10,
   step: 0.1,
-  value: 3,
-});
-
-const wobblyMix = new RangeControl("MIX", "general-controls", {
-  min: 0,
-  max: 1,
-  step: 0.01,
-  value: 0.5,
+  value: 1,
 });
 
 const sliceX = new RangeControl("SLICE_X", "general-controls", {
@@ -70,13 +49,41 @@ const sliceX = new RangeControl("SLICE_X", "general-controls", {
   value: 0,
 });
 
-// Wobbly Controls
-
-const sliceY = new RangeControl("SLICE_Y", "wobbly-controls", {
+const sliceY = new RangeControl("SLICE_Y", "general-controls", {
   min: 0,
   max: 10,
   step: 1,
   value: 0,
+});
+
+// Wobbly Controls
+
+const wobblyMix = new RangeControl("MIX", "wobbly-controls", {
+  min: 0,
+  max: 1,
+  step: 0.01,
+  value: 0.5,
+});
+
+const wobblyBoost = new RangeControl("Boost", "wobbly-controls", {
+  min: 0,
+  max: 10,
+  step: 0.1,
+  value: 3,
+});
+
+const a = new RangeControl("A", "wobbly-controls", {
+  min: 0,
+  max: Math.PI / 4,
+  step: Math.PI / 100000,
+  value: Math.PI / 200,
+});
+
+const b = new RangeControl("B", "wobbly-controls", {
+  min: 0,
+  max: 0.01,
+  step: 0.00005,
+  value: 0.05,
 });
 
 // Perlin Controls
@@ -85,6 +92,13 @@ const perlinMix = new RangeControl("MIX", "perlin-controls", {
   max: 1,
   step: 0.01,
   value: 0.5,
+});
+
+const perlinBoost = new RangeControl("Boost", "perlin-controls", {
+  min: 0,
+  max: 10,
+  step: 0.1,
+  value: 3,
 });
 
 const perlinColSize = new RangeControl("Col Size", "perlin-controls", {
@@ -97,6 +111,40 @@ const perlinColSize = new RangeControl("Col Size", "perlin-controls", {
 const perlinRowSize = new RangeControl("Row Size", "perlin-controls", {
   min: 0,
   max: 1,
+  step: 0.01,
+  value: 0.5,
+});
+
+// Patches
+
+const patchRowOffset = new RangeControl("Row Offset", "patches-controls", {
+  min: 0,
+  max: 1,
+  step: 0.01,
+  value: 0.5,
+});
+
+const patchColOffset = new RangeControl("Row Offset", "patches-controls", {
+  min: 0,
+  max: 1,
+  step: 0.01,
+  value: 0.5,
+});
+
+const patchMaxAmplitude = new RangeControl(
+  "Max Amplitude",
+  "patches-controls",
+  {
+    min: 0,
+    max: 10,
+    step: 0.1,
+    value: 3,
+  }
+);
+
+const patchNoiseCap = new RangeControl("Noise Cap", "patches-controls", {
+  min: 0,
+  max: 2,
   step: 0.01,
   value: 0.5,
 });
@@ -163,13 +211,13 @@ const getWobblyY = (col: number, row: number, t: number) => {
   const colOnSLice = getColSlice(col);
 
   const w0 =
-    AMP *
+    wobblyBoost.value *
       Math.sin(
         A * colOnSLice + B * t + C + D * Math.sin(E * rowOnSLice + F * t + G)
       ) +
     0;
   const w1 =
-    AMP *
+    wobblyBoost.value *
       Math.sin(
         H * colOnSLice + I * t + J + K * Math.sin(L * rowOnSLice + M * t + N)
       ) +
@@ -179,13 +227,27 @@ const getWobblyY = (col: number, row: number, t: number) => {
 };
 
 const getPerlinY = (col: number, row: number) => {
-  const x = getRowSlice(row * perlinColSize.value) / COLS;
-  const y = getColSlice(col * perlinRowSize.value) / ROWS;
+  const x = getRowSlice(row * perlinRowSize.value) / COLS;
+  const y = getColSlice(col * perlinColSize.value) / ROWS;
 
   const n = noise2D(x, y);
 
   // make sure the noise is always positive
-  return (n + 1) * AMP;
+  return (n + 1) * perlinBoost.value;
+};
+
+const getPatchesY = (col: number, row: number) => {
+  const x = getRowSlice(row * perlinRowSize.value) / COLS;
+  const y = getColSlice(col * perlinColSize.value) / ROWS;
+
+  const n = noise2D(x + patchColOffset.value, y + patchRowOffset.value);
+
+  if (n + 1 < patchNoiseCap.value) {
+    return 0;
+  }
+
+  // make sure the noise is always positive
+  return (n + 1) * patchMaxAmplitude.value;
 };
 
 const mixY = (values: { value: number; weight: number }[]) => {
@@ -250,11 +312,14 @@ function draw() {
 
       const wobblyY = getWobblyY(col, row, t);
       const perlinY = getPerlinY(col, row);
+      const perlinSpikeY = getPatchesY(col, row);
 
-      const y = mixY([
-        { value: wobblyY, weight: wobblyMix.value },
-        { value: perlinY, weight: perlinMix.value },
-      ]);
+      const y =
+        mixY([
+          { value: wobblyY, weight: wobblyMix.value },
+          { value: perlinY, weight: perlinMix.value },
+          { value: perlinSpikeY, weight: 1 },
+        ]) * AMP;
 
       const cube = boxGroup.children[row * COLS + col] as THREE.Mesh;
       const geometry = new THREE.BoxGeometry(BOX_SIZE, y, BOX_SIZE);
